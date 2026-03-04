ae3d4ca01af20ba8c59fc17b0550dab141922973
Ahmeddouhm <a.douha0@gmail.com>
2026-03-04 08:15:43 +0200

    Add 1st Task "Number Guessing Game"

--- Number Guessing Game/Program.cs
+++ Number Guessing Game/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿Console.WriteLine("=========================================");
+Console.WriteLine("     Welcome To Number Guessing Game     ");
+Console.WriteLine("=========================================");
+
+Random randomObject = new Random();
+bool isFinished = false;
+
+
+while (!isFinished)
+{
+    int range, guesses = 1, guess = 0, secret;
+
+    // input validation 
+    Console.Write("Enter Max Range : ");
+    while (!int.TryParse(Console.ReadLine(), out range))
+    {
+        Console.Write("Enter Max Range : ");
+    }
+
+    secret = randomObject.Next(1, range + 1);
+
+
+    while (guess != secret)
+    {
+        Console.Write("Enter Guess : ");
+        if (!int.TryParse(Console.ReadLine(), out guess))
+            continue;
+
+        if (guess > secret)
+        {
+            Console.WriteLine("You are too high !");
+            guesses++;
+        }
+        else if (guess < secret)
+        {
+            Console.WriteLine("You are too low !");
+            guesses++;
+        }
+    }
+
+    Console.WriteLine($"Yoy are correct in {guesses} guesses");
+
+    Console.Write("Wanna play again ? (Y/N) => ");
+    
+    if (Console.ReadLine().ToUpper() == "N")
+    {
+        isFinished = true;
+    }
+}
